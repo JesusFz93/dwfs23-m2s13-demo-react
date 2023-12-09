e2301aacb0b4238062444ba26398ea9e095a8e72
Jesus <jesus.fdz93@gmail.com>
2023-12-09 13:10:17 -0600

    eventos

--- src/pages/ListPage.jsx
+++ src/pages/ListPage.jsx
@@ -4,6 +4,14 @@ import NavBar from "../components/NavBar";
 const ListPage = () => {
   const listaUsuarios = ["Marlen", "Linda", "Jorge", "Mayra"];
 
+  const saludar = () => {
+    console.log("Hola mundo");
+  };
+
+  const mouseover = () => {
+    console.log("Pasaste por encima de la etiqueta");
+  };
+
   return (
     <>
       <NavBar />
@@ -13,9 +21,14 @@ const ListPage = () => {
         </header>
         <main className="row">
           <div className="col">
-            <button type="button" className="btn btn-success">
+            <button type="button" className="btn btn-success" onClick={saludar}>
               Obtener informacion
             </button>
+            <img
+              src="https://http2.mlstatic.com/D_NQ_NP_657047-MLM50695240516_072022-O.webp"
+              alt="Perrito"
+              onMouseEnter={mouseover}
+            />
             <ListaUsuarios listaUsuarios={listaUsuarios} />
           </div>
         </main>
